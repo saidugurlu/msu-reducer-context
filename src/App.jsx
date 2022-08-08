cb1154787ff97e6916f0cf82305ad36bbc0ed1d9
saidugurlu <ugurlumsaid@gmx.de>
2022-08-08 10:19:15 +0200

    minimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.scss'
 
 function App() {
  
 
   return (
     <div className="App">
-     
+     <h1>Site with useContext/useReducer</h1>
+    <p>test</p>
     </div>
   )
 }
