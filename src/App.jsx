4315ff62c55024d2e1e9389d2aa76fb96d5d8c7c
saidugurlu <ugurlumsaid@gmx.de>
2022-08-08 10:46:00 +0200

    useContext works

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.scss'
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+import './App.scss';
 
 function App() {
- 
+	const { count, setCount } = useContext(AppContext);
 
-  return (
-    <div className="App">
-     <h1>Site with useContext/useReducer</h1>
-    <p>test</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Site with useContext/useReducer</h1>
+			<p>test: {count}</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { AppProvider } from './AppContext';
- 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <AppProvider>
-        <App />
-    </AppProvider>
+	<AppProvider>
+		<App />
+	</AppProvider>
 );
