d83d61265f6ced3257f9998a62bc7debbc8bf4d0
saidugurlu <ugurlumsaid@gmx.de>
2022-08-08 10:52:24 +0200

    buttons work with useContext

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,20 @@ function App() {
 		<div className="App">
 			<h1>Site with useContext/useReducer</h1>
 			<p>test: {count}</p>
+      <div className="buttonArea">
+                <button
+                    className="decrease"
+                    onClick={() => setCount(count - 1)}
+                >
+                    -
+                </button>
+                <button
+                    className="increase"
+                    onClick={() => setCount(count + 1)}
+                >
+                    +
+                </button>
+            </div>
 		</div>
 	);
 }
