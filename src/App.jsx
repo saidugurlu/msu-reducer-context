629d5c8d1f55949242af2b490182a0969e23aa8a
saidugurlu <ugurlumsaid@gmx.de>
2022-08-09 10:09:10 +0200

    show german nouns with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { useContext } from 'react';
-import { AppContext } from './AppContext';
-import './App.scss';
+import { useContext } from "react";
+import { AppContext } from "./AppContext";
+import "./App.scss";
 
 function App() {
-	const { state, dispatch } = useContext(AppContext);
+  const { state, dispatch } = useContext(AppContext);
 
-	return (
-		<div className="App">
-			<h1>Site with useContext/useReducer</h1>
-			<p>test: {state.count}</p>
+  return (
+    <div className="App">
+      <h1>Site with useContext/useReducer</h1>
+      <p>test: {state.count}</p>
       <div className="buttonArea">
-                <button
-                    className="decrease"
-                    onClick={() => dispatch({type: "decreaseCount"})}
-                >
-                    -
-                </button>
-                <button
-                    className="increase"
-                    onClick={() => dispatch({type:"increaseCount"})}
-                >
-                    +
-                </button>
+        <button
+          className="decrease"
+          onClick={() => dispatch({ type: "decreaseCount" })}
+        >
+          -
+        </button>
+        <button
+          className="increase"
+          onClick={() => dispatch({ type: "increaseCount" })}
+        >
+          +
+        </button>
+      </div>
+      <hr />
+      <p>There are {state.germanNouns.length} nouns.</p>
+      <div className="germanNounArea">
+        {state.germanNouns.map((noun) => {
+          return (
+            <div className="germanNoun" key={noun.id}>
+              {noun.singular}
             </div>
-            <hr/>
-		<p>There are {state.germanNouns.length} nouns.</p>
-		</div>
-	);
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
